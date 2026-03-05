0063a66df61e5a8ae82cd6da01a8573bbe13f310
Faruk Spahic <farukspahictz@gmail.com>
2026-03-05 20:03:43 +0100

    Initialized tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState<string>("");
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/status")
-      .then((response) => response.json())
-      .then((data) => {
-        setMessage(data.text);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  return (
-    <div>
-      <h1>Frontend calling Backend</h1>
-      <p>Message from Go: {message || "Loading..."}</p>
-    </div>
-  );
+  return <div className="text-4xl font-bold">hello</div>;
 }
 
 export default App;
